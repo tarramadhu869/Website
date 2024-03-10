fb226c4eda315a0043b5492af5828262393863b4
unknown <tarramadhuri869@gmail.com>
2024-03-10 16:32:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let imgSlider = document.querySelector('.history img')
+let indicator = document.querySelectorAll('.footer .slideIndicator span')
+let imgs = ['images/peak_2.jpg', 'images/rectangle_23.jpg', 'images/rectangle_23_copy.jpg'];
+let time = 3000
+let i = 0;
+
+
+function slideImages() {
+    indicator.forEach((el) => {
+        el.classList.remove("active");
+    })
+    imgSlider.src = imgs[i]
+    indicator[i].classList.add("active");
+    
+    
+    if(i < imgs.length-1) {
+        i++
+    } else {
+        i = 0
+    }
+
+    setTimeout(() => {
+        slideImages()
+    }, time);
+}
+
+console.log(indicator[2])
+
+// calls the slideImages once the pages loads
+window.onload = slideImages;
